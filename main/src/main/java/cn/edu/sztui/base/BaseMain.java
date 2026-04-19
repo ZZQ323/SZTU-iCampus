8985a8f8e1f2dc0cdcd49f737a64c4aefbc5b5e7
zzq323 <zzq32323@gmail.com>
2026-04-20 01:03:28 +1000

    信息补充

--- main/src/main/java/cn/edu/sztui/base/BaseMain.java
+++ main/src/main/java/cn/edu/sztui/base/BaseMain.java
@@ -1,6 +1,7 @@
 package cn.edu.sztui.base;
 
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
